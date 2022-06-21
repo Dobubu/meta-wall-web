7e6a7c19bb758c430de9d5b580b40130fa24bcda
mischa.huang <mischa.huang@creativearktech.com>
2022-06-21 11:12:45 +0800

    init: pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 import eslintPlugin from 'vite-plugin-eslint';
 import path from 'path';
 import WindiCSS from 'vite-plugin-windicss';
@@ -12,7 +13,45 @@ export default defineConfig({
       '@': path.resolve(__dirname, '/src'),
     },
   },
-  plugins: [vue(), eslintPlugin({ cache: true }), WindiCSS()],
+  plugins: [
+    vue(),
+    VitePWA({
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Meta Wall Web',
+        short_name: 'Meta PWA',
+        description: 'Meta PWA',
+        theme_color: '#ffffff',
+        display: 'fullscreen',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+    eslintPlugin({ cache: true }),
+    WindiCSS(),
+  ],
   server: {
     port: 3001,
     host: '0.0.0.0',
