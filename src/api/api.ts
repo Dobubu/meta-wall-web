6bbff3f2db49e599a664512e5f9f0571e5fbd7b6
mischa <mischa0619@gmail.com>
2024-11-10 16:06:32 +0800

    f refactor: api structure

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,34 +2,50 @@ import axios from 'axios';
 
 import { useAuth } from '@/service/useAuth';
 
-const authService = useAuth();
+export const useLocalhost = import.meta.env.VITE_USE_LOCALHOST === 'true';
 
-export const useLocalhost = true;
+export const getApiUrl = (state = 'http') => {
+  if (state === 'ws') {
+    if (process.env.NODE_ENV === 'production') {
+      return `wss${import.meta.env.VITE_API_URL}`;
+    }
+    return 'ws://localhost:3001';
+  } else {
+    return `http${import.meta.env.VITE_API_URL}`;
+  }
+};
+
+export const getApiNetworkUrl = (state = 'http') =>
+  state === 'ws'
+    ? `ws${import.meta.env.VITE_API_NETWORK_URL}`
+    : `http${import.meta.env.VITE_API_NETWORK_URL}`;
+
+const authService = useAuth();
 
 const instance = axios.create({
   baseURL:
     process.env.NODE_ENV === 'production'
-      ? 'https://meta-wall-backend.onrender.com/'
+      ? getApiUrl()
       : useLocalhost
-      ? 'http://127.0.0.1:3001'
-      : 'http://192.168.168.16:3001',
+      ? getApiUrl()
+      : getApiNetworkUrl(),
 });
 
 instance.interceptors.request.use(
-  config => {
+  (config) => {
     if (config.headers) {
       config.headers['Authorization'] = `Bearer ${authService.getToken()}`;
     }
     return config;
   },
-  error => Promise.reject(error),
+  (error) => Promise.reject(error),
 );
 
 instance.interceptors.response.use(
-  res => {
+  (res) => {
     return res.data;
   },
-  error => Promise.reject(error.response.data),
+  (error) => Promise.reject(error.response.data),
 );
 
 export default instance;
