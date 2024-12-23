a15863ad3eca2301add6e7bd613e0a536bb18a04
mischa <mischa0619@gmail.com>
2024-12-23 17:36:43 +0800

    f refactor: api structure

--- src/compositions/handleErrorAsync.ts
+++ src/compositions/handleErrorAsync.ts
@@ -1,18 +1,22 @@
-type AsyncCallback<T, A> = (cb_payload?: A) => Promise<T>;
+import type { AxiosResponse } from 'axios';
 
-type HandleErrorAsyncOptions<T, A> = {
+type AsyncCallback<T, A = void> = (cb_payload?: A) => Promise<AxiosResponse<T>>;
+
+interface HandleErrorAsyncOptions<T, A = void> {
   callback: AsyncCallback<T, A>;
   payload?: any;
+  onSuccess?: (result: AxiosResponse<T>) => T;
   onError?: (error: any) => void;
   onFinally?: () => void;
-};
+}
 
-export const handleErrorAsync = async <T, A>({
+export const handleErrorAsync = async <T, A = void>({
   callback,
   payload,
+  onSuccess,
   onError,
   onFinally,
-}: HandleErrorAsyncOptions<T, A>) => {
+}: HandleErrorAsyncOptions<T, A>): Promise<T | AxiosResponse<T, any>> => {
   try {
     // const _cb = cb_payload ? cb(cb_payload) : cb();
     // let res;
@@ -24,7 +28,8 @@ export const handleErrorAsync = async <T, A>({
     // }
 
     const res = await callback();
-    return res;
+
+    return onSuccess ? onSuccess(res) : res;
   } catch (e: any) {
     console.error(`[ฅ^•ﻌ•^ฅ] API error occurred - ${e.error.statusCode}。`, e.message);
     if (onError) {
